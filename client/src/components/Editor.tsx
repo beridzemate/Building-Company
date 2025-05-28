8c8696fe689a44ea681341268a369d9b552c1464
mateberidze101 <beridzemate545@gmail.com>
2025-05-28 21:36:19 +0400

    rewrite project on next

--- client/src/components/Editor.tsx
+++
@@ -1,7 +0,0 @@
-// 
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
-export default function Editor({ value, onChange }: { value: string, onChange: (val: string) => void }) {
-  return <ReactQuill value={value} onChange={onChange} />;
-}
--- client/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Welcome to Build Company</h1>
-      </div>
-    );
-  }
